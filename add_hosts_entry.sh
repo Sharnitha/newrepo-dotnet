0bbf55d30cdaa625f32776a61b702f779ab04710
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-14 00:06:25 +0530

    Update add_hosts_entry.sh

--- add_hosts_entry.sh
+++ add_hosts_entry.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# echo "6.12.16.11 SharniDe" >> /etc/hosts
+echo "6.12.16.11 SharniDe" >> /etc/hosts
 
-echo "Starting script execution"
-date
-# Your script logic here
-echo "Script execution completed"
 
