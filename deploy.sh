9e4ca648ded64526c5ddc6797c44279261315376
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-16 21:06:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,17 @@
 
 GITHUB_RUN_ID=$1
 
+LOG_DEFA=${{ secrets.LOG }}
+
+sed -i "s|#{LOG_DEFAULT}#|$LOG_DEFA|g" appsettings.json
+
 # Fetch secrets from Azure Key Vault
-DATABASE_CONNECTION_STRING=$(az keyvault secret show --name "logLevel-default" --vault-name "keyvaulttest1506" --query "value" -o tsv)
-API_KEY=$(az keyvault secret show --name "micro" --vault-name "keyvaulttest1506" --query "value" -o tsv)
+# DATABASE_CONNECTION_STRING=$(az keyvault secret show --name "logLevel-default" --vault-name "keyvaulttest1506" --query "value" -o tsv)
+# API_KEY=$(az keyvault secret show --name "micro" --vault-name "keyvaulttest1506" --query "value" -o tsv)
 
 # Replace placeholders in appsetting.json with fetched secrets
-sed -i "s|#{__logLevel__}#|$DATABASE_CONNECTION_STRING|g" appsettings.json
-sed -i "s|#{__microsoft__}#|$API_KEY|g" appsettings.json
+# sed -i "s|#{__logLevel__}#|$DATABASE_CONNECTION_STRING|g" appsettings.json
+# sed -i "s|#{__microsoft__}#|$API_KEY|g" appsettings.json
 
 IMAGE_TAG=githubcisharni.azurecr.io/demoenv:${GITHUB_RUN_ID}
 
@@ -20,7 +24,7 @@ name: containerapps
 resourceGroup: sharnitha-poc
 type: Microsoft.App/containerApps
 properties:
-    managedEnvironmentId: /subscriptions/8da5ea31-eccb-4d99-8a1a-437ea5504220/resourceGroups/sharnitha-poc/providers/Microsoft.App/managedEnvironments/managedEnvironment-sharnithapoc-aa5a
+    managedEnvironmentId: /subscriptions/8da5ea31-eccb-4d99-8a1a-437ea5504220/resourceGroups/sharnitha-poc/providers/Microsoft.App/managedEnvironments/managedEnvironment-sharnithapoc-b20f
     configuration:
         activeRevisionsMode: single
        
@@ -29,10 +33,8 @@ properties:
         - image: $IMAGE_TAG
           name: githubcisharni
           env:
-          - name: DatabaseConnectionString
-            value: "$DATABASE_CONNECTION_STRING"
-          - name: ApiKey
-            value: "$API_KEY"
+          - name: LOG_DEFAULT
+            value: "$LOG_DEFA"
           resources:
               cpu: 2  
               memory: 4Gi  
