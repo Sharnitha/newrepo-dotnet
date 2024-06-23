8ab2feb0764971ebe8dd414be41a8d9a58042bde
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-23 18:28:57 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,5 @@ set -e
 # Start SSH service
 service ssh start
 
-# Login to Azure using managed identity or service principal
-# # az login --service-principal -u $DOCKER_LOGIN -p $DOCKER_PASSWORD --tenant $DOCKER_TENANT
-# az login --service-principal -u c03d7f87-710f-41e6-951e-1b48aebeb8f0 -p  -t 406f6fb2-e087-4d29-9642-817873fddc4c
-
-# Fetch and replace placeholders in appsettings.json
-SECRET_DEFAULT=$(az keyvault secret show --name DefaultLogLevel --vault-name $KEYVAULT_NAME --query value -o tsv)
-SECRET_MICROSOFT=$(az keyvault secret show --name MicrosoftLogLevel --vault-name $KEYVAULT_NAME --query value -o tsv)
-
-sed -i "s|{__DEFAULT__}|$SECRET_DEFAULT|g" /app/appsettings.json
-sed -i "s|{__MICROSOFT__}|$SECRET_MICROSOFT|g" /app/appsettings.json
-
 # Start the application
 exec dotnet dotnet-folder.dll
