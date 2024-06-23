438a12ca1d0c71525461eb85c2e9448e0dedce15
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-23 17:39:49 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 service ssh start
 
 # Login to Azure using managed identity or service principal
-az login --service-principal -u $DOCKER_LOGIN -p $DOCKER_PASSWORD --tenant $DOCKER_TENANT
+# az login --service-principal -u $DOCKER_LOGIN -p $DOCKER_PASSWORD --tenant $DOCKER_TENANT
+az login --service-principal -u c03d7f87-710f-41e6-951e-1b48aebeb8f0 -p $DOCKER_PASSWORD -t 406f6fb2-e087-4d29-9642-817873fddc4c
 
 # Fetch and replace placeholders in appsettings.json
 SECRET_DEFAULT=$(az keyvault secret show --name DefaultLogLevel --vault-name $KEYVAULT_NAME --query value -o tsv)
