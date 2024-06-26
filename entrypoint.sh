82aad8a6d60ac82bf8b00649658c2d9a66c733e0
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-26 17:54:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
- 
+LOG=$LOG
+MICRO=$MICROSOFT
 # Start SSH service
-sed -i "s|{__LOG__}|LOG|g" appsettings.json
-sed -i "s|{__MICRO__}|MICROSOFT|g" appsettings.json
+sed -i "s|{__LOG__}|$LOG|g" appsettings.json
+sed -i "s|{__MICRO__}|$MICROSOFT|g" appsettings.json
 
 # Start the application
-exec dotnet dotnet-folder.dll
+dotnet run dotnet-folder.dll
