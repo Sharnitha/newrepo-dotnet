d09b423f03166983a9f42ced4bdae08cff8bd1ed
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-28 18:38:31 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
  
 service ssh start
 
-MICRO=$MICROSOFT
-echo $MICROSOFT
+# MICRO=$MICROSOFT
+# echo $MICROSOFT
 # Start SSH service
 sed -i "s|__LOGDEF__|$LOGGING|g" appsettings.json
 sed -i "s|__MICRO__|$MICROSOFT|g" appsettings.json 
