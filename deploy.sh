4d8bd5d3038b097cd5aac9551c422c8b09a98ed1
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-07-01 00:31:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+GITHUB_RUN_ID=$1
+
+# Fetch secrets from Azure Key Vault
+# SHARNILOGKEYVAULT=$(az keyvault secret show --name "KEYVAULTSHARNITHALOG" --vault-name "dockeyvault280824" --query "value" -o tsv)
+# echo $SHARNILOGKEYVAULT
+# API_KEY=$(az keyvault secret show --name "micro" --vault-name "keyvaulttest1506" --query "value" -o tsv)
+
+# Replace placeholders in appsetting.json with fetched secrets
+# sed -i "s|#{__logLevel__}#|$DATABASE_CONNECTION_STRING|g" appsettings.json
+# sed -i "s|#{__microsoft__}#|$API_KEY|g" appsettings.json
+
+IMAGE_TAG=githubcisharni.azurecr.io/demoenv:${GITHUB_RUN_ID}
+
+# Define backend.yaml content dynamically with fetched secrets 
+cat <<EOF > backend.yaml
 kind: containerapp
 location: East US
 name: contaiinerapps13
