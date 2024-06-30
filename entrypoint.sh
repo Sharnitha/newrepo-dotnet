f2159c02fc2f545130f3ad79f09b397d9172a2a6
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-06-30 20:07:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ service ssh start
 # MICRO=$MICROSOFT  
 # echo $MICROSOFT
 # Start SSH service
-sed -i "s|__LOGDEF__|$SHARNILOG|g" appsettings.json 
-sed -i "s|__MICRO__|$SHARNITHAMICROSOFT|g" appsettings.json  
-sed -i "s|__BASEURL__|$BASEKEYURL|g" appsettings.json 
-sed -i "s|__APIKEY__|$APIBASEURL|g" appsettings.json
+sed -i "s|{__LOGDEF__}|$SHARNILOG|g" appsettings.json 
+sed -i "s|{__MICRO__}|$SHARNITHAMICROSOFT|g" appsettings.json  
+sed -i "s|{__BASEURL__}|$BASEKEYURL|g" appsettings.json 
+sed -i "s|{__APIKEY__}|12345abcdefg|g" appsettings.json
 exec dotnet dotnet-folder.dll
