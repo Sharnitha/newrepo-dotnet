bfbf1b40d8bbb0183c94d732e9099cd3e6b543db
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-08-06 12:32:02 +0530

    Update backendentrypoint.sh

--- backendentrypoint.sh
+++ backendentrypoint.sh
@@ -8,6 +8,6 @@ service ssh start
 # Start SSH service
 # sed -i "s|{__LOGDEF__}|$SHARNILOG|g" appsettings.json  
 sed -i "s|{__LOGDMICROSOFT__}|$LOGLEVELMICROSOFT|g" appsettings.json
-sed -i "s|{__BASEURL__}|$BASEKEYURL|g" appsettings.json 
+sed -i "s|{__BASEURL__}|@Microsoft.KeyVault(SecretUri=https://keyvaultname0108.vault.azure.net/secrets/KEY01/71cb391771fd45c1a96e84aebb416095)|g" appsettings.json 
 # sed -i "s|{__APIKEY__}|$APIBASEURL|g" appsettings.json
 exec dotnet dotnet-folder.dll
