bbdd3ba99f03a017cca7e8d34eb95b1e1de9a826
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2024-09-03 15:06:52 +0530

    Update backendentrypoint.sh

--- backendentrypoint.sh
+++ backendentrypoint.sh
@@ -2,12 +2,13 @@
 set -e
  
 service ssh start
- 
+
+exec dotnet dotnet-folder.dll
 # MICRO=$MICROSOFT  
 # echo $MICROSOFT
 # Start SSH service
 # sed -i "s|{__LOGDEF__}|$SHARNILOG|g" appsettings.json  
-sed -i "s|{__LOGDMICROSOFT__}|$LOGLEVELMICROSOFT|g" appsettings.json
-sed -i "s|{__BASEURL__}|$BASEKEYURL|g" appsettings.json 
-# sed -i "s|{__APIKEY__}|$APIBASEURL|g" appsettings.json
-exec dotnet dotnet-folder.dll
+# sed -i "s|{__LOGDMICROSOFT__}|$LOGLEVELMICROSOFT|g" appsettings.json
+# sed -i "s|{__BASEURL__}|$BASEKEYURL|g" appsettings.json 
+# # sed -i "s|{__APIKEY__}|$APIBASEURL|g" appsettings.json
+# exec dotnet dotnet-folder.dll
